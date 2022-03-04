53a8ec1ec3a63036d5f0bfb8e7b8444d54d50354
alejorom <morojela-github@yahoo.com>
2022-03-04 16:48:13 -0500

    Agregar constants.dart

---
+++ lib/constants/constants.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+const labelTextStyle = TextStyle(
+  fontSize: 18.0,
+  color: Color(0xFF8D8E98),
+);
+
+const double buttonContainerHeight = 60;
+const activeCardColor = Color(0xFF1D1E33);
+const inactiveCardColor = Color(0xFF111328);
+const buttonContainerColor = Color(0xFFEB1555);
--- lib/pages/input_page.dart
+++ lib/pages/input_page.dart
@@ -1,11 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import '../constants/constants.dart';
 import '../widgets/widgets.dart';
 
-const double buttonContainerHeight = 60;
-const activeCardColor = Color(0xFF1D1E33);
-const inactiveCardColor = Color(0xFF111328);
-const buttonContainerColor = Color(0xFFEB1555);
 enum Gender {
   male,
   female,
